c97ed3ac12669f9cabc304c5392adf0240f7558f
anand maharana <anandmaharana427@gmail.com>
2025-10-12 07:44:16 +0530

    feat: add health check endpoint to server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import Express from 'express';
 
 const app = Express();
-let port = 4000;
+const port = 4000;
 
-
+app.get('/health', (_, res) => {
+  try {
+    return res.status(200).json({
+      message: 'Server health is good',
+      statusCode: 200,
+      data: null,
+    });
+  } catch (error: unknown) {
+    console.error('Something went wrong while check Health check');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Application listen on port : ${port}`);
